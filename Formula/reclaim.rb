ace614b4a2b5db759787538fd21b7e7b6b16bc45
petetanton <peter_tanton@yahoo.co.uk>
2025-01-09 13:54:41 +0000

    Update sha

--- Formula/reclaim.rb
+++ Formula/reclaim.rb
@@ -1,12 +1,12 @@
 class Reclaim < Formula
   desc "Command line tool for reclaim.ai"
   homepage "https://github.com/petetanton/reclaim-cli"
-  url "https://github.com/petetanton/reclaim-cli/archive/refs/tags/v0.0.12.tar.gz"
-  sha256 "37cad4cbc2711f30cace9f9556824bc938ec25cbca5961afe8559d3b36ce8ed6"
-  license ""
+  url "https://github.com/petetanton/reclaim-cli/releases/download/v0.0.12/reclaim-cli_0.0.12_darwin_arm64.zip"
+  sha256 "7f010bb7b331922eb65cb7cd6fad8dae5a9e432f4aa1a0342578ad662b8d8053"
+  license 
 
   def install
-    bin.install "reclaim-cli_v0.0.11" => "reclaim"
+    bin.install "reclaim-cli_v0.0.12" => "reclaim"
   end
 
   def post_install
@@ -15,15 +15,6 @@ class Reclaim < Formula
 
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ops-watcher`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
