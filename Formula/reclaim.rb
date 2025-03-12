50e0098e3fd76200ec744df9a3aaaa7308804064
Pete Tanton <11050440+petetanton@users.noreply.github.com>
2025-03-12 10:30:25 +0000

    set correct version

--- Formula/reclaim.rb
+++ Formula/reclaim.rb
@@ -1,12 +1,12 @@
 class Reclaim < Formula
   desc "Command line tool for reclaim.ai"
   homepage "https://github.com/petetanton/reclaim-cli"
-  url "https://github.com/petetanton/reclaim-cli/releases/download/v0.0.13/reclaim-cli_0.0.13_darwin_arm64.zip"
+  url "https://github.com/petetanton/reclaim-cli/releases/download/v0.0.14/reclaim-cli_0.0.14_darwin_arm64.zip"
   sha256 "94eb6cb5f1e4da18a6fe502ef1caddbf510c182e2734fd66bb0312024176a54d"
   license 
 
   def install
-    bin.install "reclaim-cli_v0.0.12" => "reclaim"
+    bin.install "reclaim-cli_v0.0.14" => "reclaim"
   end
 
   def post_install
